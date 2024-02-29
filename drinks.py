4c32ed621d8439369f248c330dd0228c8d1271fb
gusfoley <115668629+gusfoley@users.noreply.github.com>
2024-02-29 13:56:29 -0500

    Entry Point Script
    This module provides the drink recipes entry point script.

---
+++ drinks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# drink_recipe_project/drinks.py
+
+"""This module provides Drink Recipes entry point script."""
+
+from drink_recipes.main import main
+
+if __name__ == "__main__":
+    main()
