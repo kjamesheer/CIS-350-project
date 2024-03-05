ce6534042182272d1042d3eb9356189c92af43dd
kjamesheer <144500144+kjamesheer@users.noreply.github.com>
2024-03-04 22:32:48 -0500

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# rpcontacts/database.py
+
+"""This module provides a database connection."""
+
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtSql import QSqlDatabase
+from PyQt5 import QtSql
+
+def createDB():
+    """Create and open a database connection."""
+    connection = QSqlDatabase.addDatabase("QSQLITE")
+    connection.setDatabaseName('drinks_test.db')
+
+    if not connection.open():
+        QMessageBox.warning(
+            None,
+            "RP Contact",
+            f"Database Error: {connection.lastError().text()}",
+        )
+        return False
+
+    query = QtSql.QSqlQuery()
+
+    query.exec("create table mixertable(id int primary key, "
+               "name varchar(40), type varchar(40))")
+
+    return True
+
+createDB()
