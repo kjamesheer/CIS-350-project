4cda9c473d8634ee3774119040e77582dae97d74
kjamesheer <144500144+kjamesheer@users.noreply.github.com>
2024-03-13 22:27:28 -0400

    our project release 1 code
    our project release 1 code

--- streamlit.py
+++ streamlit.py
@@ -2,11 +2,14 @@ import streamlit as st
 import sqlite3
 import pandas as pd
 
+
 def main():
     connect = sqlite3.connect('Liquor_Database.db')
     cursor = connect.cursor()
 
-    st.markdown("<h2 style='text-align: center; color: white;'>Liquor Recipe Ingredient Search System</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color:"
+                " white;'>Liquor Recipe Ingredient Search System</h2>",
+                unsafe_allow_html=True)
 
     # Create a two-column layout
     col1, col2 = st.columns(2)
@@ -15,58 +18,70 @@ def main():
     with col1:
         type_selection = st.selectbox("Select Type", ["Select Type", "Liquor", "Mixer", "Garnish", "Glass"])
 
-    # Create the second box for selecting specific types(minor) based on the first selection
-    with col2:
         if type_selection == "Liquor":
-            st.write("Liquor Table Data")
-
+            st.write("Liquor Table Data:")
             query = "SELECT * FROM Spirit;"
             df = pd.read_sql_query(query, connect)
-
-            # Display the dataframe in Streamlit
             st.dataframe(df)
 
-            # Run the SQL query to get all unique spirit values
+        elif type_selection == "Mixer":
+            st.write("Mixer Table Data:")
+            query = "SELECT * FROM Mixer;"
+            df = pd.read_sql_query(query, connect)
+            st.dataframe(df)
+
+        elif type_selection == "Garnish":
+            st.write("Garnish Table Data:")
+            query = "SELECT * FROM Garnish;"
+            df = pd.read_sql_query(query, connect)
+            st.dataframe(df)
+
+        elif type_selection == "Glass":
+            st.write("Glass Table Data:")
+            query = "SELECT * FROM GlassType;"
+            df = pd.read_sql_query(query, connect)
+            st.dataframe(df)
+
+        else:
+            pass
+
+    # Create the second box for selecting specific types(minor)
+    # based on the first selection
+    with col2:
+        if type_selection == "Liquor":
+
             query2 = "SELECT DISTINCT sprt_name FROM Spirit;"
             cursor.execute(query2)
             liquor_items = cursor.fetchall()
 
-            # Create a Streamlit selectbox and populate it with options
             selected_spirit = st.selectbox("Select Liquor:", [str(liquor[0]) for liquor in liquor_items], index=None)
 
-            query3 = "SELECT DISTINCT sprt_id FROM Spirit;"
-            cursor.execute(query3)
-            liquor_ids = cursor.fetchall()
+            # Initialize selected_spirit_id with a default value
+            selected_spirit_id = ""
 
-            selected_spirit_id = st.selectbox("Selected ID:", [str(liquor[0]) for liquor in liquor_ids], index=None)
+            # Check if a liquor is selected before querying the ID
+            if selected_spirit:
+                query3 = (f"SELECT DISTINCT sprt_id FROM Spirit"
+                          f" WHERE sprt_name = '{selected_spirit}';")
+                cursor.execute(query3)
+                selected_spirit_row = cursor.fetchone()
 
+                if selected_spirit_row is not None:
+                    selected_spirit_id = selected_spirit_row[0]
 
-            query4 = f"""
-                    SELECT 
-                    DrinkRecipe.drnk_id AS drnk_id_recipe, 
-                    DrinkRecipe.picture AS picture_recipe, 
-                    DrinkRecipe.drnk_name AS drnk_name_recipe, 
-                    DrinkRecipe.sprt_id AS sprt_id_recipe, 
-                    DrinkRecipe.Garnish_id AS Garnish_id_recipe, 
-                    DrinkRecipe.mix_id AS mix_id_recipe, 
-                    DrinkRecipe.glass_id AS glass_id_recipe, 
-                    DrinkRecipe.AlcContent AS AlcContent_recipe, 
-                    DrinkRecipe.description AS description_recipe, 
-                    Spirit.sprt_id AS sprt_id_spirit, 
-                    Spirit.sprt_name AS sprt_name, 
-                    Spirit.spr_alc_content AS spr_alc_content
-                    FROM DrinkRecipe 
-                    JOIN Spirit ON Spirit.sprt_id = DrinkRecipe.sprt_id 
-                    WHERE DrinkRecipe.sprt_id = '{selected_spirit_id}';
-            """
+            # Query based on the selected spirit ID
+            query4 = (f"SELECT * FROM DrinkRecipe WHERE sprt_id"
+                      f" = '{selected_spirit_id}';")
             df = pd.read_sql_query(query4, connect)
-            st.dataframe(df)
+
+            if selected_spirit_id and df.empty:
+                st.write("No recipe with this ingredient."
+                         " Please select another ingredient")
+            elif selected_spirit_id:
+                st.write("Recipes:")
+                st.dataframe(df)
 
         elif type_selection == "Mixer":
-            st.write("Mixer Table Data")
-            query = "SELECT * FROM Mixer;"
-            df = pd.read_sql_query(query, connect)
-            st.dataframe(df)
 
             query2 = "SELECT DISTINCT mix_name FROM Mixer;"
             cursor.execute(query2)
@@ -74,11 +89,28 @@ def main():
 
             selected_mixer = st.selectbox("Select Mixer:", [str(mixer[0]) for mixer in mixer_items], index=None)
 
+            selected_mixer_id = ""
+
+            if selected_mixer:
+                query3 = (f"SELECT DISTINCT mix_id FROM Mixer"
+                          f" WHERE mix_name = '{selected_mixer}';")
+                cursor.execute(query3)
+                selected_mixer_row = cursor.fetchone()
+
+                if selected_mixer_row is not None:
+                    selected_mixer_id = selected_mixer_row[0]
+
+            query4 = (f"SELECT * FROM DrinkRecipe WHERE"
+                      f" mix_id = '{selected_mixer_id}';")
+            df = pd.read_sql_query(query4, connect)
+            if selected_mixer_id and df.empty:
+                st.write("No recipe with this ingredient."
+                         " Please select another ingredient")
+            elif selected_mixer_id:
+                st.write("Recipes:")
+                st.dataframe(df)
+
         elif type_selection == "Garnish":
-            st.write("Garnish Table Data")
-            query = "SELECT * FROM Garnish;"
-            df = pd.read_sql_query(query, connect)
-            st.dataframe(df)
 
             query2 = "SELECT DISTINCT Garnish_name FROM Garnish;"
             cursor.execute(query2)
@@ -86,23 +118,61 @@ def main():
 
             selected_garnish = st.selectbox("Select Garnish:", [str(garnish[0]) for garnish in garnish_items], index=None)
 
+            selected_garnish_id = ""
+
+            if selected_garnish:
+                query3 = (f"SELECT DISTINCT Garnish_id FROM Garnish"
+                          f" WHERE Garnish_name = '{selected_garnish}';")
+                cursor.execute(query3)
+                selected_garnish_row = cursor.fetchone()
+
+                if selected_garnish_row is not None:
+                    selected_garnish_id = selected_garnish_row[0]
+
+            query4 = (f"SELECT * FROM DrinkRecipe WHERE"
+                      f" Garnish_id = '{selected_garnish_id}';")
+            df = pd.read_sql_query(query4, connect)
+            if selected_garnish_id and df.empty:
+                st.write("No recipe with this ingredient."
+                         " Please select another ingredient")
+            elif selected_garnish_id:
+                st.write("Recipes:")
+                st.dataframe(df)
+
         elif type_selection == "Glass":
-            st.write("Glass Table Data")
-            query = "SELECT * FROM GlassType;"
-            df = pd.read_sql_query(query, connect)
-            st.dataframe(df)
 
             query2 = "SELECT DISTINCT glass_name FROM GlassType;"
             cursor.execute(query2)
             glass_items = cursor.fetchall()
 
             selected_glass = st.selectbox("Select Glass:", [str(glass[0]) for glass in glass_items], index=None)
-            #if not None:
-                #st.write(f"Selected Glass: {selected_glass}")"""
+
+            selected_glass_id = ""
+
+            if selected_glass:
+                query3 = (f"SELECT DISTINCT glass_id FROM GlassType"
+                          f" WHERE glass_name = '{selected_glass}';")
+                cursor.execute(query3)
+                selected_glass_row = cursor.fetchone()
+
+                if selected_glass_row is not None:
+                    selected_glass_id = selected_glass_row[0]
+
+            query4 = (f"SELECT * FROM DrinkRecipe WHERE glass_id"
+                      f" = '{selected_glass_id}';")
+            df = pd.read_sql_query(query4, connect)
+            if selected_glass_id and df.empty:
+                st.write("No recipe with this ingredient."
+                         " Please select another ingredient")
+            elif selected_glass_id:
+                st.write("Recipes:")
+                st.dataframe(df)
+
         else:
             pass
 
     connect.close()
+
 
 if __name__ == "__main__":
     main()
